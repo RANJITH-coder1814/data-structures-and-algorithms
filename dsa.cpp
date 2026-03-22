89bd5c1b170ca86d3cc63d0e25247785fde33a04
Ranjith.HL <ranjithranjith2747@gmail.com>
2026-03-22 21:05:06 +0530

    Create dsa.cpp

---
+++ dsa.cpp
@@ -0,0 +1,20 @@
+// Problem: Two Sum
+// Platform: LeetCode
+// Link: https://leetcode.com/problems/two-sum/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> twoSum(vector<int>& nums, int target) {
+    unordered_map<int, int> mp;
+
+    for (int i = 0; i < nums.size(); i++) {
+        int diff = target - nums[i];
+
+        if (mp.count(diff)) {
+            return {mp[diff], i};
+        }
+        mp[nums[i]] = i;
+    }
+    return {};
+}
